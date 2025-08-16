7d28c27052a47ef39a211fa9b428d5d546782e61
DorothyDanger <harriotpower@hotmail.com>
2025-08-16 19:27:32 +0100

    Game loop + display added.

--- AsteroidGame/main.py
+++ AsteroidGame/main.py
@@ -1,6 +1,16 @@
 import pygame
-import constants
+#import constants
+from constants import *
 def main():
+    pygame.init()
+    displayWindow = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(displayWindow, color="black") # Fill the screen with black
+        pygame.display.flip()
+    
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
