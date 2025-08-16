129a3eebc21c8397a373cd4bee076da187f3c5f4
DorothyDanger <harriotpower@hotmail.com>
2025-08-16 19:39:57 +0100

    Added an FPS limit of 60

--- AsteroidGame/main.py
+++ AsteroidGame/main.py
@@ -3,8 +3,11 @@ import pygame
 from constants import *
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     displayWindow = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        dt = clock.tick(60) / 1000.0  # Limit to 60 FPS
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
