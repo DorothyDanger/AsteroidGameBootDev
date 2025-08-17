5d86fab5956f789a0853d5820501aa839769d55d
DorothyDanger <harriotpower@hotmail.com>
2025-08-17 04:31:15 +0100

    Added new group and container for shots.

--- AsteroidGame/main.py
+++ AsteroidGame/main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -13,12 +14,14 @@ def main():
     update_group = pygame.sprite.Group()
     draw_group = pygame.sprite.Group()
     asteroid_group = pygame.sprite.Group()
+    shot_group = pygame.sprite.Group()
 
     # Add objects to the sprite groups
     Asteroid.containers = (update_group, draw_group, asteroid_group)
     Player.containers = (update_group, draw_group)
     AsteroidField.containers = (update_group)
-
+    Shot.containers = (update_group, draw_group, shot_group)
+    
     # Create instances of asteroids and player + display window
     asteroid_field = AsteroidField()
     displayWindow = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
